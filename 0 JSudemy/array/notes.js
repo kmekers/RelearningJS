e61ead61eed392a84456e54d10257ace9a9fdb60
koen <koenmekers@outlook.com>
2021-12-13 03:12:05 +0100

    update

---
+++ 0 JSudemy/array/notes.js
@@ -0,0 +1,4 @@
+const notes = ["note 1", "note 2", "note 3"];
+
+console.log(notes.length);
+console.log(notes[0]);
---
+++ 0 JSudemy/array/todo.js
@@ -0,0 +1,11 @@
+let toDo = [
+  "js leren",
+  "css-grid leren",
+  "positioning leren",
+  "vh etc leren",
+  "wordpress cursus doen",
+];
+console.log(`je hebt ${toDo.length} to do's`);
+console.log(
+  `De eerste todo is ${toDo[0]} de tweede is ${toDo[toDo.length - 2]} `
+);
