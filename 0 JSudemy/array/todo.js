1bb315058ee7d0b2735160cf49426450fa8e277f
koen <koenmekers@outlook.com>
2021-12-18 02:34:32 +0100

    update

--- 0 JSudemy/array/todo.js
+++ 0 JSudemy/array/todo.js
@@ -22,13 +22,27 @@ const todos = [
 ];
 
 let deleteTodo = function (todos, todotext) {
-  let i = todos.findIndex(function (dontknowwhythisworks) {
-    return dontknowwhythisworks.text.toLowerCase() === todotext.toLowerCase();
+  let i = todos.findIndex(function (todo) {
+    return todo.text.toLowerCase() === todotext.toLowerCase();
   });
   if (i > -1) {
     todos.splice(i, 1);
   }
 };
 
-deleteTodo(todos, "buy food");
-console.log(todos);
+const getThingstoDo = function (todos) {
+  return (todos.filter = todos.filter(function (todo, ) {
+    const istrue = todo.completed === false;
+    // shorthand = retun !todo.completed
+    return istrue;
+  }));
+};
+
+//const isTrue = todos.completed.includes(true);
+
+//return isTrue === true;
+
+console.log(getThingstoDo(todos));
+
+//deleteTodo(todos, "buy food");
+//console.log(todos);
