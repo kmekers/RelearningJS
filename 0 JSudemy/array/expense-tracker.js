0b66aabbe35f99e3638c4b511403c8b0a8f2dcca
koen <koenmekers@outlook.com>
2021-12-24 16:01:02 +0100

    update

--- 0 JSudemy/array/expense-tracker.js
+++ 0 JSudemy/array/expense-tracker.js
@@ -1,9 +1,15 @@
 let count = 0;
+let totalIncome = 0;
 const account = {
   name: "andrew mead",
   expenses: [],
+  incomeAll: [],
   addExpense: function (Expensetitle, amount) {
     this.expenses.push({ Expensetitle: Expensetitle, amount: amount });
+  },
+
+  addIncome: function (incomeTitle, amount2) {
+    this.incomeAll.push({ incomeTitle: incomeTitle, amount2: amount2 });
   },
   getAccountSummary: function () {
     totalExpense = 0;
@@ -11,11 +17,17 @@ const account = {
     this.expenses.forEach((expense) => {
       totalExpense = expense.amount + totalExpense;
     });
-    return `${this.name} has ${totalExpense}`;
+    this.incomeAll.forEach(function (income) {
+      totalIncome = totalIncome + income.amount2;
+    });
+    return `${this.name} has budget off ${
+      totalIncome - totalExpense
+    } zoveel in   ${totalIncome}inkomen  zoveel ${totalExpense} in uitgaven`;
   },
 };
 
 account.addExpense("coffee", 2);
 account.addExpense("drugs", 50);
-console.log(account.expenses);
+account.addIncome("loon", 1800);
+console.log(account.incomeAll);
 console.log(account.getAccountSummary());
