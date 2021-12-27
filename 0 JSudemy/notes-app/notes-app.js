2812989fe6554ab1a59ce987c60b78002abc2c87
koen <koenmekers@outlook.com>
2021-12-27 18:09:37 +0100

    update

--- 0 JSudemy/notes-app/notes-app.js
+++ 0 JSudemy/notes-app/notes-app.js
@@ -1,3 +1,17 @@
+const notes = [
+  {
+    title: "my next trip",
+    body: "I would like to go to Spain",
+  },
+  {
+    title: "Habbits to work on",
+    body: "Exercise. Eating a bit better.",
+  },
+  {
+    title: "Office modification",
+    body: "Get a new seat",
+  },
+];
 // DOM - Document Object Model
 
 // Query and remove
@@ -12,3 +26,7 @@ ps.forEach(function (p) {
   // console.log(p.textContent)
   // p.remove()
 });
+
+const newParagraph = document.createElement("p");
+newParagraph.textContent = "this is a new element from Javascript";
+document.querySelector("body").appendChild(newParagraph);
--- 0 JSudemy/todo-app/todos.js
+++ 0 JSudemy/todo-app/todos.js
@@ -1,7 +1,52 @@
-const paragraphs = document.querySelectorAll("p");
+const todos = [
+  {
+    text: "Order cat food",
+    completed: true,
+  },
+  {
+    text: "Clean kitchen",
+    completed: false,
+  },
+  {
+    text: "Buy food",
+    completed: true,
+  },
+  {
+    text: "Do work",
+    completed: false,
+  },
+  {
+    text: "Exercise",
+    completed: true,
+  },
+];
+
+const completeTodos = todos.filter(function (todo) {
+  return !todo.completed;
+});
+
+const hoeveel = completeTodos.length;
+
+todos.forEach(function (todo) {
+  const printTodo = document.createElement("p");
+  printTodo.textContent = todo.text;
+  document.querySelector("body").appendChild(printTodo);
+});
+
+const zoveel = document.createElement("h1");
+zoveel.textContent = `${hoeveel}`;
+document.querySelector("body").appendChild(zoveel);
+
+// you have 2 todos left (p element)
+// add a p for each todo above
+
+// eerst todos geraken, dan filteren op completed, index fixen en weergeven
+
+/* const paragraphs = document.querySelectorAll("p");
 
 paragraphs.forEach(function (paragraph) {
-  if (paragraph.textContent.includes("the")) {
+  if (paragraph.textContent.includes("the")) {y
     paragraph.remove();
   }
 });
+ */
