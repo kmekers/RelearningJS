d1d0af8b43872eb43a55c215cca53f523a8633b4
koen <koenmekers@outlook.com>
2022-01-04 03:20:33 +0100

    update

--- 0 JSudemy/todo-app/todo-app.js
+++ 0 JSudemy/todo-app/todo-app.js
@@ -26,25 +26,28 @@ const renderdTodos = function (todos, filters) {
   const filteredTodos = todos.filter(function (todo) {
     return todo.text.toLowerCase().includes(filters.searchText);
   });
+  const incompleteTodos = filteredTodos.filter(function (todo) {
+    return !todo.completed;
+  });
+
+  document.querySelector("#todos").innerHTML = "";
+
+  const summary = document.createElement("h2");
+  summary.textContent = `You have ${incompleteTodos.length} todos left`;
+  document.querySelector("#todos").appendChild(summary);
+
+  todos.forEach(function (todo) {
+    const p = document.createElement("p");
+    p.textContent = todo.text;
+    document.querySelector("body").appendChild(p);
+  });
 };
-
-const incompleteTodos = todos.filter(function (todo) {
-  return !todo.completed;
-});
-
-const summary = document.createElement("h2");
-summary.textContent = `You have ${incompleteTodos.length} todos left`;
-document.querySelector("body").appendChild(summary);
-
-todos.forEach(function (todo) {
-  const p = document.createElement("p");
-  p.textContent = todo.text;
-  document.querySelector("body").appendChild(p);
-});
+renderdTodos(todos, filters);
 
 document.querySelector("#add-todo").addEventListener("click", function () {
   console.log("todo added");
 });
+
 document.querySelector("#search-text").addEventListener("input", function (e) {
   filters.searchText = e.target.value;
   renderdTodos(todos, filters);
