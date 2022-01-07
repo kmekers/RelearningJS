d3e215316891fcb7745219d80dcd65905ba73bf6
koen <koenmekers@outlook.com>
2022-01-07 03:20:42 +0100

    update

--- Studycards/studycard.js
+++ Studycards/studycard.js
@@ -1,7 +1,14 @@
 // inputfield dat input ergens op slaat
 // En die onder elkaar opslaat
-// wissen per note 
+// wissen per note
 // zoeken per vak?
 // filter per titel
 // button nieuw
-
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
